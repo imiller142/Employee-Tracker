9b51991458ece2a191b099d6d5fd808a3b3e8886
Ian Miller <imiller142@gmail.com>
2024-01-30 17:55:59 -0600

    finished tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,13 @@ DROP TABLE IF EXISTS role;
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+
  );
 
  CREATE TABLE department (
@@ -16,5 +22,6 @@ CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     tile VARCHAR(30),
     salary DECIMAL,
-    department_id REFERENCES department(id) ON DELETE SET NULL
- )
+    department_id INTEGER NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+ );
