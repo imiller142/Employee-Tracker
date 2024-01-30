897c62674e29416ed0200b570d7b896a8d15c8a1
Ian Miller <imiller142@gmail.com>
2024-01-30 16:43:03 -0600

    created tables

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+﻿DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+ );
+
+ CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+ );
+
+ CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tile VARCHAR(30),
+    salary DECIMAL,
+    department_id REFERENCES department(id) ON DELETE SET NULL
+ )
