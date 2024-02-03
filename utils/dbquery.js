2fea29e8ea35bc1e38227e3b44e6110504d27fab
Ian Miller <imiller142@gmail.com>
2024-02-03 13:23:08 -0600

    finished database calls

--- utils/dbquery.js
+++ utils/dbquery.js
@@ -12,58 +12,95 @@ export class Queries {
 
     constructor() {
 
-    }
-
-    connect() {
-        return mysql.createConnection({
+        this.db = mysql.createConnection({
             host: 'localhost',
             user: 'root',
             password: 'password1',
             database: 'employees'
         });
+
     }
 
 
+
     getAllEmployees() {
-        const db = this.connect()
-        db
+        
+        this.db
             .promise()
             .query(`SELECT
             employee.id,
             employee.first_name,
             employee.last_name,
-            roles.title
-            department.name AS department
+            roles.title,
+            department.name as department,
             CONCAT(e2.first_name, ' ', e2.last_name) AS manager
             FROM employee
-            LEFT JOIN roles ON emp_role_id = employee.emp_role_id
-            LEFT JOIN department on department.id = emp_role.department_id
+            LEFT JOIN roles ON roles.id = employee.role_id
+            LEFT JOIN department on department.id = roles.department_id
             LEFT JOIN employee AS e2 ON e2.id = employee.manager_id
             `
             )
             .then(([rows, fields]) => {
                 console.table(rows);
             })
-            .catch(console.log)
-            .then(() => db.end());
+            .catch(console.log);
     }
 
     getAllRoles() {
-        const db = this.connect();
-        db
+        this.db
             .promise()
-            .query(`SELECT * FROM roles`)
+            .query(`SELECT
+                    roles.title,
+                    roles.id,
+                    roles.salary,
+                    department.name as department
+                    from roles
+                    left join department on roles.department_id = department.id`)
             .then(([rows, fields]) => {
                 console.table(rows);
             })
+            .catch(console.log);
+    }
+
+    getAllDepartments() {
+        this.db
+            .promise()
+            .query(`Select * from department`)
+            .then(([rows, fields]) => {
+                console.table(rows);
+            })
+            .catch(console.log);
+    }
+
+    addDepartment(name) {
+        this.db
+            .promise()
+            .query(`Insert into department (name)
+                    values (?)`, [name])
+            .catch(console.log);
+    }
+
+    addRole(name, salary, department_id) {
+        this.db
+            .promise()
+            .query(`insert into roles (title, salary, department_id)
+                    values (?,?,?)`, [name, salary, department_id])
+            .catch(console.log);
+    }
+
+    updateEmployee(role, id) {
+        this.db
+            .promise()
+            .query(`update employee
+                    set role_id = ?
+                    where id = ?`, [role, id])
             .catch(console.log)
-            .then(() => db.end());
     }
 }
 
 const data = new Queries();
 
-data.getAllRoles();
+data.updateEmployee(1, 1);
 data.getAllEmployees();
 
 // class Queries {
